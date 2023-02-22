d318429f99f74a0c26e030fe360779cbc4010a0d
GMercury <99487197+GMercury@users.noreply.github.com>
2023-02-22 18:49:58 +0300

    Update 0x07-react_redux_action_creator_normalizr

---
+++ 0x07-react_redux_action_creator_normalizr/task_5/dashboard/src/actions/notificationActionTypes.js
@@ -0,0 +1,10 @@
+
+// Create actions types for the notification list
+
+export const MARK_AS_READ = 'MARK_AS_READ';
+export const SET_TYPE_FILTER ='SET_TYPE_FILTER';
+
+export const NotificationTypeFilters = {
+    DEFAULT: 'DEFAULT',
+    URGENT: 'URGENT'
+};
