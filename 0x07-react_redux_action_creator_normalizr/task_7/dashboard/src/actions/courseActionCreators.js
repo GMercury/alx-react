58d6648101fbe26824aec9c429b3e5e69f3312ce
GMercury <99487197+GMercury@users.noreply.github.com>
2023-02-22 18:54:00 +0300

    Update 0x07-react_redux_action_creator_normalizr

---
+++ 0x07-react_redux_action_creator_normalizr/task_7/dashboard/src/actions/courseActionCreators.js
@@ -0,0 +1,16 @@
+// Async Action Creators
+
+import { SELECT_COURSE, UNSELECT_COURSE } from './courseActionTypes';
+
+
+export function selectCourse(index) {
+    return { type: SELECT_COURSE, index }
+};
+
+export const boundselectCourse = (index) => dispatch(selectCourse(index));
+
+export function unSelectCourse(index) {
+    return { type: UNSELECT_COURSE, index }
+};
+
+export const boundunSelectCourse = (index) => dispatch(unSelectCourse(index))
